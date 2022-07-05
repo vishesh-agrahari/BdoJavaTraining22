b0d11e196dacd7c5afc31499559bd3833114f0c1
Vishesh <visheshagrahari@bdo.in>
2022-07-05 11:38:03 +0530

    new project addedd

---
+++ IOCDemoWithAnnotation/src/com/demo/main/BeanFcatoryDemo.java
@@ -0,0 +1,24 @@
+package com.demo.main;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+
+import com.demo.Student;
+
+public class BeanFcatoryDemo {
+
+	public static void main(String[] args) {
+
+		Resource r = new ClassPathResource("applicationContext.xml");
+		BeanFactory f = new XmlBeanFactory(r);
+
+		Student s = (Student) f.getBean("mybean");
+		
+		// student s= new Student()  s.show
+		s.show();
+
+	}
+
+}
